0a20d56a88c3dea848fbebb2f6d2ba882d65225b
Kristjan <krix@planet.ee>
2021-08-27 22:44:44 +0300

    set up Promise.allSettled()

--- src/context/context.js
+++ src/context/context.js
@@ -33,13 +33,32 @@ const GithubProvider = ({ children }) => {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
       //repos
-      axios(`${rootUrl}/users/${login}/repos?per_page=100`)
-        .then((response) => setRepos(response.data))
+      /*
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => setRepos(response.data))
         .catch((error) => console.log(error));
       //followers
-      axios(`${followers_url}?per_page=100`)
-        .then((response) => setFollowers(response.data))
+      
+        axios(`${followers_url}?per_page=100`).then((response) => setFollowers(response.data))
         .catch((error) => console.log(error));
+*/
+
+      //we use Promise.allSettled, to load all promises at the same time
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = 'fulfilled';
+
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
     } else {
       //unsuccessful response
       toggleError(true, 'there is no user with that username');
